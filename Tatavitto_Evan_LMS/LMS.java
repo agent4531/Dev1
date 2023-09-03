acd3207dcfd2784a84b0f3b54d6cb36b14342c3d
Evan <evantatavitto@gmail.com>
2023-09-03 16:04:46 -0400

    finsh mod 2 LMS

--- Tatavitto_Evan_LMS/LMS.java
+++ Tatavitto_Evan_LMS/LMS.java
@@ -9,13 +9,48 @@ public class LMS {
 
     static File infile = new File(LMS.class.getResource("input.txt").getPath());
     static List<List<String>> cBooks = new ArrayList<List<String>>();
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) {
 
         try {
+            String urequest = "0";
+            boolean quit = false;
             readFile();
-            newBook();
-            showBook();
+
+            while(!quit){
+                sOption();
+                urequest = reader.readLine();
+                switch (urequest) {
+                    case ("N"):
+                    case ("n"): {
+                        System.out.println("Starting New Book Selection!");
+                        newBook();
+                        break;
+                    }
+                    case ("R"):
+                    case ("r"): {
+                        System.out.println("Starting Remove Book Selection!");
+                        rmBook();
+                        break;
+                    }
+                    case ("A"):
+                    case ("a"): {
+                        System.out.println("Starting View Book Collection!");
+                        showBook();
+                        break;
+                    }
+                    case ("Q"):
+                    case ("q"):
+                        System.out.println("Quitting now!");
+                        quit = true;
+                        break;
+                    default:
+                        System.out.println("Sorry That's Not A Valid Selection! Try Again!");
+                        Thread.sleep(3000);
+                        break;
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -45,8 +80,6 @@ public class LMS {
     }
     public static void newBook() throws Exception{
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         System.out.println("What is the Title?");
         String title = reader.readLine();
 
@@ -59,16 +92,48 @@ public class LMS {
         cBooks.get(cBooks.size() - 1).add(String.valueOf(newId()));
         cBooks.get(cBooks.size() - 1).add(title);
         cBooks.get(cBooks.size() - 1).add(author);
-    }
-    public static void findId(){
 
+        Thread.sleep(3000);
     }
-    public static void rmBook(){
-
-    }
-    public static void showBook(){
-        for (int i = 0; i < cBooks.size(); i++){
-            System.out.println(cBooks.get(i).get(0) + "," + cBooks.get(i).get(1) + "," + cBooks.get(i).get(2));
+    public static int findId() throws Exception {
+        String rmId = "";
+        System.out.println("Which Book are you looking to remove? (ID only)");
+        rmId = reader.readLine();
+        for (int i=0;cBooks.size()>i; i++){
+            if (cBooks.get(i).get(0).equals(rmId)){
+                return i;
+            }
         }
+        return -1;
+    }
+    public static void rmBook() throws Exception{
+
+        boolean indexReal = false;
+        int index = -1;
+        while(!indexReal) {
+            index = findId();
+            if ( index >= 0 && index < cBooks.size()) {
+                System.out.println("Removing!");
+                cBooks.remove(index);
+                indexReal =true;
+                Thread.sleep(3000);
+            }else {
+                System.out.println("Thats not a Real ID Try Again!");
+            }
+        }
+    }
+    public static void showBook() throws Exception{
+        for (int i = 0; i < cBooks.size(); i++){
+            System.out.println(cBooks.get(i).get(0) + ", " + cBooks.get(i).get(1) + ", " + cBooks.get(i).get(2));
+        }
+        Thread.sleep(3000);
+    }
+
+    public static void sOption(){
+        System.out.println("\nPlease select what to do!");
+        System.out.println("Make a New book (N)");
+        System.out.println("Remove a book (R)");
+        System.out.println("See All books (A)");
+        System.out.println("Quit (Q)");
     }
 }
